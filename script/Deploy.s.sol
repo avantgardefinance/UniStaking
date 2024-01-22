4cbd633f6a94f373e4139cc72555a7390ce0f871
Sebastian Lorenz <fubhy@fubhy.com>
2024-01-22 13:02:26 +0100

    chore: add deploy script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {UniStaker} from "scopelift/src/UniStaker.sol";
+import {ERC20VotesMock} from "scopelift/test/mocks/MockERC20Votes.sol";
+import {ERC20Fake} from "scopelift/test/fakes/ERC20Fake.sol";
+
+contract Deploy is Script {
+    uint256 constant public DEPLOYER = uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+    address public constant REWARDS_TOKEN = 0x83e83Eb0bCc4B922e5032DA9c34fcCAf00b87432; 
+    address public constant GOVERNANCE_TOKEN = 0xf4d010117e1c1296075569F54a4768b176b68b6A;
+    address public constant UNI_STAKER = 0xFb6E5742285f3Fd43a616a02b774351Ea574ba3f;
+
+    function run() external {
+        vm.startBroadcast(DEPLOYER);
+
+        ERC20Fake rewardsToken = new ERC20Fake{salt: "rewards"}();
+        vm.label(address(rewardsToken), "Rewards Token");
+
+        ERC20VotesMock govToken = new ERC20VotesMock{salt: "governance"}();
+        vm.label(address(govToken), "Governance Token");
+
+        address rewardsNotifier = address(0xaffab1ebeef);
+        vm.label(rewardsNotifier, "Rewards Notifier");
+
+        UniStaker uniStaker = new UniStaker{salt: "unistaker"}(rewardsToken, govToken, rewardsNotifier);
+        vm.label(address(uniStaker), "UniStaker");
+
+        vm.stopBroadcast();
+    }
+}

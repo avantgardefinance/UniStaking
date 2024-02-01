4d25eb78795aae640ad9d5b0a16e80d7b50e8be2
Sebastian Lorenz <fubhy@fubhy.com>
2024-02-01 22:41:25 +0100

    fix: subgraph deployment

--- contracts/scripts/Seed.s.sol
+++ contracts/scripts/Seed.s.sol
@@ -8,7 +8,7 @@ import {ERC20Fake} from "scopelift/test/fakes/ERC20Fake.sol";
 
 contract Seed is Script {
     function run() external {
-      stake(0, 321e18, address(GOVERNANCE_TOKEN));
+      stake(0, 321e18, address(0xdeadbeef));
       stake(1, 123e18, address(0xb0b));
       stake(2, 456e18, address(0xa11c3));
     }

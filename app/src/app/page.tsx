be70f40112104bf6217b7b2956c208540aa00b4e
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2024-02-05 16:00:44 +0100

    feat: add faq

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,15 @@
+import { FAQ } from "@/components/FAQ"
 import { StakeCards } from "@/components/stake-cards"
 
 export default function IndexPage() {
   return (
-    <section>
-      <StakeCards totalStakedUni={18n} rewardsWeth={20n ** 10n} />
-    </section>
+    <div className="space-y-4">
+      <section>
+        <StakeCards totalStakedUni={18n} rewardsWeth={20n ** 10n} />
+      </section>
+      <section>
+        <FAQ />
+      </section>
+    </div>
   )
 }

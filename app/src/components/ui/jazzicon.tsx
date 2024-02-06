746dcb1f1e914ec9fdb941c6810943e4c5836d37
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2024-02-06 13:10:33 +0100

    fix: cr fixes

--- app/src/components/ui/jazzicon.tsx
+++ app/src/components/ui/jazzicon.tsx
@@ -1,9 +1,9 @@
 import metemaskJazzicon from "@metamask/jazzicon"
-import React from "react"
+import React, { useMemo } from "react"
 import type { Address } from "viem"
 
 export function Jazzicon({ address, size }: { address: Address; size: number }) {
-  const jazziconData = metemaskJazzicon(size, parseInt(address.slice(2, 10), 16))
+  const jazziconData = useMemo(() => metemaskJazzicon(size, parseInt(address.slice(2, 10), 16)), [address, size])
 
   return (
     <div

54f77d0ce9acbd44b8b1afc25e12d682da554c05
Ivan Herger <ivan@herger.org>
2024-02-07 17:31:11 +0100

    feat: update subgraph

--- subgraph/generated/schema.ts
+++ subgraph/generated/schema.ts
@@ -1498,6 +1498,113 @@ export class UniStakerHistory extends Entity {
   }
 }
 
+export class AdminSet extends Entity {
+  constructor(id: Bytes) {
+    super();
+    this.set("id", Value.fromBytes(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save AdminSet entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.BYTES,
+        `Entities of type AdminSet must have an ID of type Bytes but the id '${id.displayData()}' is of type ${id.displayKind()}`,
+      );
+      store.set("AdminSet", id.toBytes().toHexString(), this);
+    }
+  }
+
+  static loadInBlock(id: Bytes): AdminSet | null {
+    return changetype<AdminSet | null>(
+      store.get_in_block("AdminSet", id.toHexString()),
+    );
+  }
+
+  static load(id: Bytes): AdminSet | null {
+    return changetype<AdminSet | null>(store.get("AdminSet", id.toHexString()));
+  }
+
+  get id(): Bytes {
+    let value = this.get("id");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set id(value: Bytes) {
+    this.set("id", Value.fromBytes(value));
+  }
+
+  get oldAdmin(): Bytes {
+    let value = this.get("oldAdmin");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set oldAdmin(value: Bytes) {
+    this.set("oldAdmin", Value.fromBytes(value));
+  }
+
+  get newAdmin(): Bytes {
+    let value = this.get("newAdmin");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set newAdmin(value: Bytes) {
+    this.set("newAdmin", Value.fromBytes(value));
+  }
+
+  get blockNumber(): BigInt {
+    let value = this.get("blockNumber");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set blockNumber(value: BigInt) {
+    this.set("blockNumber", Value.fromBigInt(value));
+  }
+
+  get blockTimestamp(): BigInt {
+    let value = this.get("blockTimestamp");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set blockTimestamp(value: BigInt) {
+    this.set("blockTimestamp", Value.fromBigInt(value));
+  }
+
+  get transactionHash(): Bytes {
+    let value = this.get("transactionHash");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set transactionHash(value: Bytes) {
+    this.set("transactionHash", Value.fromBytes(value));
+  }
+}
+
 export class FeesClaimed extends Entity {
   constructor(id: Bytes) {
     super();
--- subgraph/src/v3-factory-owner.ts
+++ subgraph/src/v3-factory-owner.ts
@@ -1,5 +1,15 @@
-import { FeesClaimed } from "../generated/schema"
-import { FeesClaimed as FeesClaimedEvent } from "../generated/V3FactoryOwner/V3FactoryOwner"
+import { AdminSet, FeesClaimed } from "../generated/schema"
+import { AdminSet as AdminSetEvent, FeesClaimed as FeesClaimedEvent } from "../generated/V3FactoryOwner/V3FactoryOwner"
+
+export function handleAdminSet(event: AdminSetEvent): void {
+  const entity = new AdminSet(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  entity.oldAdmin = event.params.oldAmin
+  entity.newAdmin = event.params.newAdmin
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+  entity.save()
+}
 
 export function handleFeesClaimed(event: FeesClaimedEvent): void {
   const entity = new FeesClaimed(event.transaction.hash.concatI32(event.logIndex.toI32()))

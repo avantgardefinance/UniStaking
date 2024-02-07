0e27b34f641dbf72b04357b8df00e1cebb49b555
Ivan Herger <ivan@herger.org>
2024-02-07 16:16:25 +0100

    feat: add deploy script

--- contracts/scripts/Deploy.s.sol
+++ contracts/scripts/Deploy.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/console2.sol";
 import {Script} from "./shared/Script.sol";
 import {UniStaker} from "scopelift/src/UniStaker.sol";
+import {V3FactoryOwner} from "scopelift/src/V3FactoryOwner.sol";
 import {ERC20VotesMock} from "scopelift/test/mocks/MockERC20Votes.sol";
 import {ERC20Fake} from "scopelift/test/fakes/ERC20Fake.sol";
 
@@ -11,21 +12,50 @@ contract Deploy is Script {
     function run() external {
         vm.startBroadcast(DEPLOYER.privateKey);
 
-        console2.log("Deploying Rewards Token Contract: %s", address(REWARDS_TOKEN));
+        console2.log(
+            "Deploying Rewards Token Contract: %s",
+            address(REWARDS_TOKEN)
+        );
         address rewardsToken = address(new ERC20Fake{salt: 0}());
         assert(rewardsToken == address(REWARDS_TOKEN));
 
-        console2.log("Deploying Governance Token Contract: %s", address(GOVERNANCE_TOKEN));
+        console2.log(
+            "Deploying Governance Token Contract: %s",
+            address(GOVERNANCE_TOKEN)
+        );
         address governanceToken = address(new ERC20VotesMock{salt: 0}());
         assert(governanceToken == address(GOVERNANCE_TOKEN));
 
-        console2.log("Deploying Uniswap Staking Contract: %s", address(UNI_STAKER));
-        address uniStaker = address(new UniStaker{salt: 0}(REWARDS_TOKEN, GOVERNANCE_TOKEN, ADMIN.addr));
+        console2.log(
+            "Deploying Uniswap Staking Contract: %s",
+            address(UNI_STAKER)
+        );
+        address uniStaker = address(
+            new UniStaker{salt: 0}(REWARDS_TOKEN, GOVERNANCE_TOKEN, ADMIN.addr)
+        );
+        assert(uniStaker == address(UNI_STAKER));
+
+        console2.log(
+            "Deploying V3 Factory Owner Contract: %s",
+            address(V3_FACTORY_OWNER)
+        );
+        address V3_FACTORY_OWNER = address(
+            new V3FactoryOwner{salt: 0}(
+                ADMIN.addr,
+                FACTORY.addr,
+                REWARDS_TOKEN,
+                10 ** 19,
+                UNI_STAKER
+            )
+        );
         assert(uniStaker == address(UNI_STAKER));
 
         vm.stopBroadcast();
 
         vm.broadcast(ADMIN.privateKey);
-        UNI_STAKER.setRewardsNotifier({_rewardsNotifier: REWARDS_NOTIFIER.addr, _isEnabled: true});
+        UNI_STAKER.setRewardsNotifier({
+            _rewardsNotifier: V3_FACTORY_OWNER,
+            _isEnabled: true
+        });
     }
 }
--- contracts/scripts/shared/Script.sol
+++ contracts/scripts/shared/Script.sol
@@ -6,6 +6,7 @@ import {StdCheats} from "forge-std/StdCheats.sol";
 import {ERC20VotesMock} from "scopelift/test/mocks/MockERC20Votes.sol";
 import {ERC20Fake} from "scopelift/test/fakes/ERC20Fake.sol";
 import {UniStaker} from "scopelift/src/UniStaker.sol";
+import {V3FactoryOwner} from "scopelift/src/V3FactoryOwner.sol";
 
 abstract contract Script is StdCheats, ForgeScript {
     struct Wallet {
@@ -13,29 +14,73 @@ abstract contract Script is StdCheats, ForgeScript {
         uint256 privateKey;
     }
 
-    string constant MNEMONIC = "test test test test test test test test test test test junk";
+    string constant MNEMONIC =
+        "test test test test test test test test test test test junk";
 
-    Wallet public DEPLOYER = Wallet({addr: vm.addr(vm.deriveKey(MNEMONIC, 0)), privateKey: vm.deriveKey(MNEMONIC, 0)});
-    Wallet public REWARDS_NOTIFIER =
-        Wallet({addr: vm.addr(vm.deriveKey(MNEMONIC, 1)), privateKey: vm.deriveKey(MNEMONIC, 1)});
-    Wallet public ADMIN = Wallet({addr: vm.addr(vm.deriveKey(MNEMONIC, 2)), privateKey: vm.deriveKey(MNEMONIC, 2)});
+    Wallet public DEPLOYER =
+        Wallet({
+            addr: vm.addr(vm.deriveKey(MNEMONIC, 0)),
+            privateKey: vm.deriveKey(MNEMONIC, 0)
+        });
+    Wallet public ADMIN =
+        Wallet({
+            addr: vm.addr(vm.deriveKey(MNEMONIC, 2)),
+            privateKey: vm.deriveKey(MNEMONIC, 2)
+        });
+    Wallet public FACTORY =
+        Wallet({
+            addr: vm.addr(vm.deriveKey(MNEMONIC, 3)),
+            privateKey: vm.deriveKey(MNEMONIC, 3)
+        });
 
     ERC20VotesMock public immutable GOVERNANCE_TOKEN =
-        ERC20VotesMock(vm.computeCreate2Address(0, hashInitCode(type(ERC20VotesMock).creationCode)));
+        ERC20VotesMock(
+            vm.computeCreate2Address(
+                0,
+                hashInitCode(type(ERC20VotesMock).creationCode)
+            )
+        );
     ERC20Fake public immutable REWARDS_TOKEN =
-        ERC20Fake(vm.computeCreate2Address(0, hashInitCode(type(ERC20Fake).creationCode)));
-    UniStaker public immutable UNI_STAKER = UniStaker(
-        vm.computeCreate2Address(
-            0, hashInitCode(type(UniStaker).creationCode, abi.encode(REWARDS_TOKEN, GOVERNANCE_TOKEN, ADMIN.addr))
-        )
-    );
+        ERC20Fake(
+            vm.computeCreate2Address(
+                0,
+                hashInitCode(type(ERC20Fake).creationCode)
+            )
+        );
+    UniStaker public immutable UNI_STAKER =
+        UniStaker(
+            vm.computeCreate2Address(
+                0,
+                hashInitCode(
+                    type(UniStaker).creationCode,
+                    abi.encode(REWARDS_TOKEN, GOVERNANCE_TOKEN, ADMIN.addr)
+                )
+            )
+        );
+    V3FactoryOwner public immutable V3_FACTORY_OWNER =
+        V3FactoryOwner(
+            vm.computeCreate2Address(
+                0,
+                hashInitCode(
+                    type(V3FactoryOwner).creationCode,
+                    abi.encode(
+                        ADMIN.addr,
+                        FACTORY.addr,
+                        REWARDS_TOKEN,
+                        10 ** 19,
+                        UNI_STAKER
+                    )
+                )
+            )
+        );
 
     constructor() {
-        vm.label(REWARDS_NOTIFIER.addr, "Rewards Notifier");
         vm.label(ADMIN.addr, "Admin UniStaker");
+        vm.label(FACTORY.addr, "Factory");
         vm.label(address(REWARDS_TOKEN), "Rewards Token");
         vm.label(address(GOVERNANCE_TOKEN), "Governance Token");
         vm.label(address(UNI_STAKER), "UniStaker");
+        vm.label(address(V3_FACTORY_OWNER), "V3FactoryOwner");
     }
 
     function dealToDerivedKey(uint32 who, uint256 amount) public {
@@ -46,10 +91,11 @@ abstract contract Script is StdCheats, ForgeScript {
         vm.stopBroadcast();
     }
 
-    function stake(uint32 who, uint256 amount, address delegatee)
-        public
-        returns (UniStaker.DepositIdentifier delegateId)
-    {
+    function stake(
+        uint32 who,
+        uint256 amount,
+        address delegatee
+    ) public returns (UniStaker.DepositIdentifier delegateId) {
         uint256 key = vm.deriveKey(MNEMONIC, who);
 
         vm.startBroadcast(key);
@@ -60,7 +106,7 @@ abstract contract Script is StdCheats, ForgeScript {
     }
 
     function distributeRewards(uint256 _amount) public {
-        vm.startBroadcast(REWARDS_NOTIFIER.privateKey);
+        // vm.startBroadcast(V3_FACTORY_OWNER.privateKey);
         REWARDS_TOKEN.mint(address(UNI_STAKER), _amount);
         UNI_STAKER.notifyRewardsAmount(_amount);
         vm.stopBroadcast();

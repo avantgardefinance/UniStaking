f2d86a5c8643474225b1e2abb7e189dae3017c4a
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2024-02-08 11:11:00 +0100

    feat: extract logic to custom field

--- app/src/components/edit-beneficiary-delegatee-dialog.tsx
+++ app/src/components/edit-beneficiary-delegatee-dialog.tsx
@@ -1,27 +1,21 @@
 "use client"
 
+import { DelegateeField, type TallyDelegatee } from "@/components/form/DelegateeField"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { BigIntDisplay } from "@/components/ui/big-int-display"
-import { Button, buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import { DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Separator } from "@/components/ui/separator"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 import { abi as abiUniStaker } from "@/lib/abi/uni-staker"
 import { uniStaker } from "@/lib/consts"
 import { useWriteContractWithToast } from "@/lib/hooks/use-write-contract-with-toast"
-import { cn } from "@/lib/utils"
 import { useQuery } from "@tanstack/react-query"
 import { Info, RotateCw } from "lucide-react"
-import Link from "next/link"
-import { useForm } from "react-hook-form"
+import { FormProvider, useForm } from "react-hook-form"
 import type { Address } from "viem"
 import { encodeFunctionData, isAddressEqual } from "viem"
-
-type TallyDelegatee = { address: Address; label: string; votesCount: bigint }
 
 const useEditBeneficiaryDelegateeDialog = () => {
   const tallyDelegateesQuery = useQuery({
@@ -98,6 +92,8 @@ const useEditBeneficiaryDelegateeForm = (
 
   const tallyDelegatee = tallyDelegatees.find((delegatee) => isAddressEqual(delegatee.address, currentDelegatee))
 
+  console.log({ currentDelegatee })
+
   const form = useForm({
     defaultValues: {
       beneficiary: currentBeneficiary,
@@ -106,10 +102,6 @@ const useEditBeneficiaryDelegateeForm = (
       delegateeOption: tallyDelegatee === undefined ? "custom" : "tally"
     }
   })
-
-  const { watch } = form
-
-  const [delegateeOption] = watch(["delegateeOption"])
 
   const onSubmit = (values: {
     beneficiary: Address | undefined
@@ -169,8 +161,7 @@ const useEditBeneficiaryDelegateeForm = (
     form,
     onSubmit: form.handleSubmit((values) => onSubmit(values)),
     error: errorWrite,
-    isPending: isPendingWrite,
-    delegateeOption
+    isPending: isPendingWrite
   }
 }
 
@@ -180,7 +171,7 @@ function EditBeneficiaryDelegateeForm({ beneficiary, delegatee, stakeId, tallyDe
   stakeId: bigint
   tallyDelegatees: Array<TallyDelegatee>
 }) {
-  const { delegateeOption, error, form, isPending, onSubmit } = useEditBeneficiaryDelegateeForm({
+  const { error, form, isPending, onSubmit } = useEditBeneficiaryDelegateeForm({
     beneficiary,
     delegatee,
     stakeId,
@@ -188,7 +179,7 @@ function EditBeneficiaryDelegateeForm({ beneficiary, delegatee, stakeId, tallyDe
   })
 
   return (
-    <Form {...form}>
+    <FormProvider {...form}>
       <form onSubmit={onSubmit} className="space-y-4">
         <div className="flex flex-col space-y-2">
           <span>ID</span>
@@ -226,144 +217,7 @@ function EditBeneficiaryDelegateeForm({ beneficiary, delegatee, stakeId, tallyDe
               </FormItem>
             )}
           />
-
-          <FormField
-            control={form.control}
-            name="delegateeOption"
-            render={({ field: delegateeOptionField }) => (
-              <FormItem className="space-y-3">
-                <FormLabel>
-                  <TooltipProvider>
-                    <Tooltip>
-                      <TooltipTrigger>
-                        <div className="flex flex-row space-x-2">
-                          <span>Delegatee</span>
-                          <Info size={16} />
-                        </div>
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>
-                          You must delegate your UNI&apos;s votesCount. The default address is your own, however there
-                          are many active delegatees to choose from. You can view delegatee profiles
-                          <Link href="https://www.tally.xyz/gov/uniswap" target="_blank">
-                            {" "}
-                            <div
-                              className={cn(
-                                buttonVariants({
-                                  variant: "link"
-                                }),
-                                "space-x-2 p-0"
-                              )}
-                            >
-                              here
-                            </div>
-                          </Link>.
-                        </p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </FormLabel>
-                <FormControl>
-                  <RadioGroup
-                    onValueChange={delegateeOptionField.onChange}
-                    defaultValue={delegateeOptionField.value}
-                    value={delegateeOptionField.value}
-                    className="flex flex-col space-y-1"
-                  >
-                    <FormItem
-                      className={cn(
-                        "flex items-center space-x-3 rounded-lg border-2 border-none border-blue-700 p-2",
-                        {
-                          "border-solid": delegateeOption === "custom"
-                        }
-                      )}
-                    >
-                      <FormControl>
-                        <RadioGroupItem value="custom" />
-                      </FormControl>
-                      <FormLabel className="w-full space-y-2 font-normal">
-                        <span>Custom address</span>
-                        <FormField
-                          control={form.control}
-                          name="customDelegatee"
-                          render={({ field: customDelegateeField }) => (
-                            <FormItem>
-                              <FormControl>
-                                <Input
-                                  {...customDelegateeField}
-                                  onChange={(...values) => {
-                                    customDelegateeField.onChange(...values)
-                                    if (delegateeOption !== "custom") {
-                                      delegateeOptionField.onChange("custom")
-                                    }
-                                  }}
-                                />
-                              </FormControl>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />
-                      </FormLabel>
-                    </FormItem>
-                    <FormItem
-                      className={cn(
-                        "flex items-center space-x-3 rounded-lg border-2 border-none border-blue-700 p-2",
-                        {
-                          "border-solid": delegateeOption === "tally"
-                        }
-                      )}
-                    >
-                      <FormControl>
-                        <RadioGroupItem value="tally" />
-                      </FormControl>
-                      <FormLabel className="w-full space-y-1 font-normal">
-                        <span>Top 30 delegatees from Tally</span>
-                        <FormField
-                          control={form.control}
-                          name="tallyDelegatee"
-                          render={({ field: fieldTallyDelegatee }) => (
-                            <FormItem>
-                              <Select
-                                onValueChange={(value) => {
-                                  fieldTallyDelegatee.onChange(value)
-                                  delegateeOptionField.onChange("tally")
-                                }}
-                                defaultValue={fieldTallyDelegatee.value}
-                              >
-                                <FormControl>
-                                  <SelectTrigger>
-                                    <SelectValue placeholder="Select" />
-                                  </SelectTrigger>
-                                </FormControl>
-                                <SelectContent>
-                                  {tallyDelegatees.map((delegatee) => (
-                                    <SelectItem
-                                      key={delegatee.address}
-                                      value={delegatee.address}
-                                    >
-                                      <div className="flex items-start">
-                                        <span>{delegatee.label}</span>
-                                      </div>
-                                      <div className="flex items-start space-x-1">
-                                        <span>Votes:</span>
-                                        <BigIntDisplay value={delegatee.votesCount} decimals={18} precision={2} />
-                                      </div>
-                                    </SelectItem>
-                                  ))}
-                                </SelectContent>
-                              </Select>
-                              <FormMessage />
-                            </FormItem>
-                          )}
-                        />
-                      </FormLabel>
-                    </FormItem>
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          <DelegateeField name="delegateeOption" tallyDelegatees={tallyDelegatees} />
           {error &&
             (
               <Alert variant="destructive">
@@ -385,6 +239,6 @@ function EditBeneficiaryDelegateeForm({ beneficiary, delegatee, stakeId, tallyDe
           </Button>
         </DialogFooter>
       </form>
-    </Form>
+    </FormProvider>
   )
 }

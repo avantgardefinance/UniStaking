99d051ced786fbdf5cb6c0ae159614c306526b68
Paweł Kędzia <67273035+KedziaPawel@users.noreply.github.com>
2024-02-13 13:15:10 +0100

    chore: lint queries

--- app/src/lib/subgraph/account-events.ts
+++ app/src/lib/subgraph/account-events.ts
@@ -1,56 +1,56 @@
 import { graphql } from "@/lib/generated/subgraph/gql"
 
 export const AccountEventsQuery = graphql(`
-    query AccountEvents($account: String!) {
-      # TODO add this: orderBy: event__blockTimestamp, orderDirection: desc
-      accountEvents(where: { account: $account}, first: 1000) {
-        event {
-          __typename
+query AccountEvents($account: String!) {
+# TODO add this: orderBy: event__blockTimestamp, orderDirection: desc
+accountEvents(where: { account: $account}, first: 1000) {
+  event {
+    __typename
+    id
+    blockTimestamp
+    ... on BeneficiaryAltered {
+      deposit {
+        owner {
           id
-          blockTimestamp
-          ... on BeneficiaryAltered {
-            deposit {
-              owner {
-                id
-              }
-              id
-            }
-            oldBeneficiary
-            newBeneficiary
-          }
-          ... on DelegateeAltered {
-            newDelegatee
-            oldDelegatee
-            deposit {
-              owner {
-                id
-              }
-              id
-            }
-          }
-          ... on StakeDeposited {
-            amount
-            deposit {
-              owner {
-                id
-              }
-              id
-            }
-          }
-          ... on RewardClaimed {
-            beneficiary
-            amount
-          }
-          ... on StakeWithdrawn {
-            amount
-            deposit {
-              owner {
-                id
-              }
-              id
-            }
-          }
         }
+        id
       }
+      oldBeneficiary
+      newBeneficiary
+    }
+    ... on DelegateeAltered {
+      newDelegatee
+      oldDelegatee
+      deposit {
+        owner {
+          id
+        }
+        id
+      }
+    }
+    ... on StakeDeposited {
+      amount
+      deposit {
+        owner {
+          id
+        }
+        id
+      }
+    }
+    ... on RewardClaimed {
+      beneficiary
+      amount
+    }
+    ... on StakeWithdrawn {
+      amount
+      deposit {
+        owner {
+          id
+        }
+        id
+      }
+    }
+  }
+}
 }
 `)

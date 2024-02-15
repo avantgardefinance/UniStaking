eaa766eeee7d7d6f52dedcfd070b6e04377f8097
KedziaPawel <67273035+KedziaPawel@users.noreply.github.com>
2024-02-15 10:31:05 +0100

    feat: add footer (#20)

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,11 +3,13 @@ import "@/styles/globals.css"
 import { Providers } from "@/components/providers/providers"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
+import { buttonVariants } from "@/components/ui/button"
 import { Toaster } from "@/components/ui/toaster"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import type { Metadata } from "next"
+import Link from "next/link"
 import { ReactNode } from "react"
 
 export const metadata: Metadata = {
@@ -29,17 +31,45 @@ export default function RootLayout({
         <meta name="theme-color" media="(prefers-color-scheme: light)" color="white" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" color="black" />
       </head>
-      <body className={cn("min-h-screen font-sans antialiased", fontSans.variable)}>
-        <div className="mx-5 md:mx-20 lg:mx-40">
-          <div className="relative flex min-h-screen flex-col pt-2">
+      <body className={cn("min-h-screen font-sans antialiased flex flex-col space-y-4", fontSans.variable)}>
+        <div className="mx-5 md:mx-20 lg:mx-40 flex-grow">
+          <div className="relative flex flex-col pt-2">
             <Providers>
               <SiteHeader />
               <div className="flex-1">{children}</div>
             </Providers>
           </div>
         </div>
-        <Toaster />
         <TailwindIndicator />
+        <Toaster />
+        <footer className="bg-blue-900 flex flex-col items-center py-2">
+          <div>
+            <Link href="/">
+              <div
+                className={cn(
+                  "space-x-2",
+                  buttonVariants({
+                    variant: "link"
+                  })
+                )}
+              >
+                <span className="text-white">Stake</span>
+              </div>
+            </Link>
+            <Link href="/history">
+              <div
+                className={cn(
+                  "space-x-2",
+                  buttonVariants({
+                    variant: "link"
+                  })
+                )}
+              >
+                <span className="text-white">History</span>
+              </div>
+            </Link>
+          </div>
+        </footer>
       </body>
     </html>
   )
